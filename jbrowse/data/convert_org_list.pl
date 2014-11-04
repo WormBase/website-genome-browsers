1ea24f573855555abf1b32d4f2ba2dca69c6fa69
Scott Cain <scott+git@scottcain.net>
2014-11-04 21:02:02 +0000

    wrote a simple script to take a carriage return limited list of organisms and creating a jbrowse organism conf file

---
+++ jbrowse/data/convert_org_list.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open LIST, "organism.list" or die $!;
+open CONF, ">organisms.conf" or die $!;
+
+while (<LIST>) {
+    chomp;
+    my $organism = $_;
+    my $nice_org = $organism;
+    $nice_org =~ s/(\S)_(\w+?)\..*/\U$1. $2/;
+   
+    print CONF "[datasets.$organism]\n";
+    print CONF "name   = $nice_org\n";
+    print CONF "url    = ?data/$organism\n\n";
+ 
+}
+
+close LIST;
+close CONF;
+
+
