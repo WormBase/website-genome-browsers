581df326ef50213df1daef12e8509751219485a2
Scott Cain <scott+git@scottcain.net>
2015-07-01 16:05:17 +0000

    a new, simple script to add Name attributes to features that need them but don't have them

---
+++ bin/add_name.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Bio::GFF3::LowLevel qw/ gff3_parse_feature gff3_format_feature /;
+use IO::Handle;
+
+my $FILEIN = $ARGV[0];
+my $FILEOUT = "$FILEIN.out";
+
+my $infh   = new IO::File "< $FILEIN";
+my $outfh   = new IO::File "> $FILEOUT";
+
+while (<$infh>) {
+    next if /^#/;
+    (print $outfh && next) if /Name=/;
+    my $f = gff3_parse_feature($_);
+
+    my $id = @{$f->{attributes}->{ID}}[0];
+    $id    = @{$f->{attributes}->{public_name}}[0]  unless $id;
+    $id    = @{$f->{attributes}->{variation}}[0]  unless $id;
+
+    unless ($id) {
+        my $target = @{$f->{attributes}->{Target}}[0];
+        if ($target =~ /(\S+)\s/) {
+            $id = $1; 
+        }
+    }
+
+    warn "No id for $_" unless $id; 
+
+    $f->{attributes}->{Name} = $id; 
+
+    print $outfh gff3_format_feature($f);
+}
+
