c8a6d72999aa3a23c6fe65242133cbf292851c6c
Scott Cain <scott+git@scottcain.net>
2018-06-08 17:57:00 +0000

    making old modencode part of the main JBrowse instance

--- jbrowse/jbrowse/plugins/wormbase-glyphs/js/main.js
+++ jbrowse/jbrowse/plugins/wormbase-glyphs/js/main.js
@@ -9,6 +9,21 @@ function (
     return declare(JBrowsePlugin, {
         constructor: function (/* args*/) {
             console.log('wormbase-glyphs plugin starting');
+
+//            setTimeout(function() {
+//                var parent = document.getElementById('dijit_layout_ContentPane_2');
+//                console.log('parent is '+parent.innerHTML);
+//                parent.innerHTML = '<a href="/"><img src="https://wormbase.org/img/logo/logo_wormbase_gradient.svg" width="200px"></a>' + parent.innerHTML;
+//               
+//            }, 25000);
+
+
+            JBrowse.afterMilestone('completely initialized', function() {
+                var parent = document.getElementById('dijit_layout_ContentPane_2');
+                //console.log('parent is '+parent.innerHTML);
+                parent.innerHTML = '<a href="/"><img src="https://wormbase.org/img/logo/logo_wormbase_gradient.svg" width="200px"></a> <br/><a href="/tools/genome/jbrowse-simple/full.html?data=data%2Fold-modencode">Looking for older modENCODE data?</a>' + parent.innerHTML;
+                
+            });
         }
     });
 });
