affb353e433a89b053f7ae99ddf6b8704f12d531
Scott Cain <scott+git@scottcain.net>
2018-06-08 19:35:14 +0000

    making jbrowse display the WB logo only when in full screen mode, and only show the 'looking for modencode' when looking at elegans

--- jbrowse/jbrowse/plugins/wormbase-glyphs/js/main.js
+++ jbrowse/jbrowse/plugins/wormbase-glyphs/js/main.js
@@ -19,9 +19,19 @@ function (
 
 
             JBrowse.afterMilestone('completely initialized', function() {
+                var url    = location.href;
+  console.log(url);
+                var mod_str ='';
+                var wb_logo = '';
+                if (url.match('PRJNA13758')) {
+                    mod_str =  '<a href="/tools/genome/jbrowse-simple/full.html?data=data%2Fold-modencode">Looking for older modENCODE data?</a>';
+                }
+                if (url.match('full.html')) {
+                    wb_logo = '<a href="/"><img src="https://wormbase.org/img/logo/logo_wormbase_gradient.svg" width="200px"></a> <br/>';
+                }
                 var parent = document.getElementById('dijit_layout_ContentPane_2');
                 //console.log('parent is '+parent.innerHTML);
-                parent.innerHTML = '<a href="/"><img src="https://wormbase.org/img/logo/logo_wormbase_gradient.svg" width="200px"></a> <br/><a href="/tools/genome/jbrowse-simple/full.html?data=data%2Fold-modencode">Looking for older modENCODE data?</a>' + parent.innerHTML;
+                parent.innerHTML = wb_logo + mod_str + parent.innerHTML;
                 
             });
         }
