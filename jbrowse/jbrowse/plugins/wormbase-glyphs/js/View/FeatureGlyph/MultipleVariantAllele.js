7b33eed606eeaf891b6f9fb5894d9e69869eb04c
Scott Cain <scott+git@scottcain.net>
2020-08-27 21:49:38 +0000

    hacky but it works! (copied drawing code from diamond and downtriangle glyphs)

--- jbrowse/jbrowse/plugins/wormbase-glyphs/js/View/FeatureGlyph/MultipleVariantAllele.js
+++ jbrowse/jbrowse/plugins/wormbase-glyphs/js/View/FeatureGlyph/MultipleVariantAllele.js
@@ -1,16 +1,12 @@
 define([
            'dojo/_base/declare',
            'dojo/_base/Color',
-           'JBrowse/View/FeatureGlyph/Box',
-           'wormbase-glyphs/View/FeatureGlyph/Diamond',
-           'wormbase-glyphs/View/FeatureGlyph/DownTriangle'
+           'JBrowse/View/FeatureGlyph/Box'
        ],
        function(
            declare,
            Color,
-           BoxGlyph,
-           DiamondGlyph,
-           DownTriangleGlyph
+           BoxGlyph
        ) {
 
 
@@ -83,30 +79,181 @@ renderSegments( context, fRect ) {
     }
 },
 
+_diaGlyph: function(context, viewInfo, feature, top, overallHeight, parentFeature, style) {
+     //inlined code from Diamond.js--ugly but it works!
+        var center_on_left = false;
+        var minwidth = 8;
+        var left  = viewInfo.block.bpToX( feature.get('start') );
+        var width = viewInfo.block.bpToX( feature.get('end') ) - left;
 
-_diaGlyph: function() {
-    console.log('in dia');
-    console.log(this.track);
-    console.log(this.browser);
-    console.log(this.config);
-    return this.__diaGlyph || ( this.__diaGlyph = new DiamondGlyph({ track: this.track, browser: this.browser, config: this.config }) );
+        if (width < minwidth) {
+            width = minwidth;
+            center_on_left = true;
+        }
+
+        style = style || lang.hitch( this, 'getStyle' );
+
+        var height = this._getFeatureHeight( viewInfo, feature );
+        if( ! height )
+            return;
+        if( height != overallHeight )
+            top += Math.round( (overallHeight - height)/2 );
+
+        // background
+        var bgcolor = style( feature, 'color' );
+        if (center_on_left) {
+            if (bgcolor) {
+                context.fillStyle = bgcolor;
+                context.beginPath();
+                context.moveTo(left-width/2,top+height/2);
+                context.lineTo(left,top);
+                context.lineTo(left+width/2,top+height/2);
+                context.lineTo(left,top+height);
+                context.closePath();
+                context.fill();
+            }
+            else {
+                context.clearRect( left-width/2, top, left+width/2, height );
+            }
+        }
+        else {
+            if( bgcolor ) {
+                context.fillStyle = bgcolor;
+                context.beginPath();
+                context.moveTo(left,top+height/2);
+                context.lineTo(left + Math.max(minwidth,width)/2,top);
+                context.lineTo(left+Math.max(minwidth,width),top+height/2);
+                context.lineTo(left + Math.max(minwidth,width)/2,top+height);
+                context.closePath();
+                context.fill();
+            }
+            else {
+                context.clearRect( left, top, Math.max(8,width), height );
+            }
+        }
+        // foreground border
+        var borderColor, lineWidth;
+        if( (borderColor = style( feature, 'borderColor' )) && ( lineWidth = style( feature, 'borderWidth')) ) {
+            if (center_on_left) {
+                context.lineWidth = lineWidth;
+                context.strokeStyle = borderColor;
+
+                context.beginPath();
+                context.moveTo(left-width/2,top+height/2);
+                context.lineTo(left,top);
+                context.lineTo(left+width/2,top+height/2);
+                context.lineTo(left,top+height);
+                context.closePath();
+                context.stroke();
+            }
+            else {
+                context.lineWidth = lineWidth;
+                context.strokeStyle = borderColor;
+
+                context.beginPath();
+                context.moveTo(left,top+height/2);
+                context.lineTo(left + Math.max(minwidth,width)/2,top);
+                context.lineTo(left+Math.max(minwidth,width),top+height/2);
+                context.lineTo(left + Math.max(minwidth,width)/2,top+height);
+                context.closePath();
+                context.stroke();
+            }
+        }
 },
 
-_dtGlyph: function() {
-    return this.__dtGlyph || ( this.__dtGlyph = new DownTriangleGlyph({ track: this.track, browser: this.browser, config: this.config }) );
+_dtGlyph: function(context, viewInfo, feature, top, overallHeight, parentFeature, style) {
+        var minwidth = 8;
+        var center_on_left = false;
+        var left  = viewInfo.block.bpToX( feature.get('start') );
+        var width = viewInfo.block.bpToX( feature.get('end') ) - left;
+
+        if (width < minwidth) {
+            width = minwidth;
+            center_on_left = true;
+        }
+
+        style = style || lang.hitch( this, 'getStyle' );
+
+        var height = this._getFeatureHeight( viewInfo, feature );
+        if( ! height )
+            return;
+        if( height != overallHeight )
+            top += Math.round( (overallHeight - height)/2 );
+
+        // background
+        var bgcolor = style( feature, 'color' );
+        if (center_on_left) {
+            if( bgcolor ) {
+                context.fillStyle = bgcolor;
+                context.beginPath();
+                context.moveTo(left-minwidth/2,top);
+                context.lineTo(left,top+height);
+                context.lineTo(left + minwidth/2 ,top);
+                context.closePath();
+                context.fill();
+            }
+            else {
+                context.clearRect( left-minwidth/2, top, left+minwidth/2, height );
+            }
+        }
+        else {
+            if( bgcolor ) {
+                context.fillStyle = bgcolor;
+                context.beginPath();
+                context.moveTo(left,top);
+                context.lineTo(left + Math.max(minwidth,width)/2,top+height);
+                context.lineTo(left + Math.max(minwidth,width),top);
+                context.closePath();
+                context.fill();
+            }
+            else {
+                context.clearRect( left, top, Math.max(8,width), height );
+            }
+        }
+
+        var borderColor, lineWidth;
+        if( (borderColor = style( feature, 'borderColor' )) && ( lineWidth = style( feature, 'borderWidth')) ) {
+            if (center_on_left) {
+                context.lineWidth = lineWidth;
+                context.strokeStyle = borderColor;
+
+                context.beginPath();
+                context.moveTo(left - minwidth/2,top);
+                context.lineTo(left ,top+height);
+                context.lineTo(left + minwidth/2,top);
+                context.closePath();
+                context.stroke();
+            }
+            else {
+                context.lineWidth = lineWidth;
+                context.strokeStyle = borderColor;
+
+                context.beginPath();
+                context.moveTo(left,top);
+                context.lineTo(left + Math.max(minwidth,width)/2,top+height);
+                context.lineTo(left + Math.max(minwidth,width),top);
+                context.closePath();
+                context.stroke();
+            }
+       }
 },
+
 
 renderSegment(context, viewInfo, segmentFeature, topPx, heightPx, parentFeature, styleFunc) {
-    if (true ) {
-        this._diaGlyph();
+    if (segmentFeature.get('type') == 'point_mutation' || 
+        segmentFeature.get('type') == 'SNV' ||
+        segmentFeature.get('type') == 'SNP') {
+        this._diaGlyph(context, viewInfo, segmentFeature, topPx, heightPx, parentFeature, styleFunc);
     }
-    else if (false) {
-        this._dtGlyph();
+    else if (segmentFeature.get('type').match('insertion')) {
+        this._dtGlyph(context, viewInfo, segmentFeature, topPx, heightPx, parentFeature, styleFunc);
     }
     else {
         this.renderBox(context, viewInfo, segmentFeature, topPx, heightPx, parentFeature, styleFunc);
     }
 }
+
+
 
 });
 });
