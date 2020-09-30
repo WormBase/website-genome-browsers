947b26f0b3fa01aca04e1d714ee6a37fc6dca304
Scott Cain <scott+git@scottcain.net>
2020-09-30 20:02:14 +0000

    trying to code around inconsistencies with circularRNA

--- jbrowse/jbrowse/plugins/wormbase-glyphs/js/View/FeatureGlyph/NcRNAGene.js
+++ jbrowse/jbrowse/plugins/wormbase-glyphs/js/View/FeatureGlyph/NcRNAGene.js
@@ -19,7 +19,7 @@ _defaultConfig: function() {
     return this._mergeConfigs(
         this.inherited(arguments),
         {
-            transcriptType: 'ncRNA',
+            transcriptType: 'ncRNA,circular_RNA',
             style: {
                 transcriptLabelFont: 'normal 10px Univers,Helvetica,Arial,sans-serif',
                 transcriptLabelColor: 'black',
