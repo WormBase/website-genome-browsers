2d07efce6e29923ac213c279df80050e4d01482b
Scott Cain <scott+git@scottcain.net>
2021-05-19 04:06:57 +0000

    fixed what looked like a logic error but still doesn't pass webpack

--- protein_schematic/url_builder/src/index.js
+++ protein_schematic/url_builder/src/index.js
@@ -1,17 +1,11 @@
 console.log('hello');
 
   const protein = 'JC8.10a';
-  let lines = featList(protein);
-  console.log(lines[0]);
-  console.log(lines[1]);
-
-
-async function featList(protein) {
+  let lines = async function featList(protein) {
     const {TabixIndexedFile} = require('@gmod/tabix');
     const {LocalFile,RemoteFile,BlobFile} = require('generic-filehandle');
 
   // use a remote file or other filehandle, note RemoteFile comes from https://github.com/GMOD/generic-filehandle
-  //const {RemoteFile} = require('generic-filehandle');
     const remoteTbiIndexed = new TabixIndexedFile({
         filehandle: new RemoteFile('https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz'),
         tbiFilehandle: new RemoteFile('https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz.tbi') // can also be csiFilehandle
@@ -19,8 +13,11 @@ async function featList(protein) {
 
     const lines = [];
     await tbiIndexed.getLines(protein,0,undefined, function(line, fileOffset) {
-    console.log(line);
-    lines.push(line);
+      console.log(line);
+      lines.push(line);
+    });
     return lines;
-  });
 }
+
+  console.log(lines[0]);
+  console.log(lines[1]);
