b2d43526ca2e163cd37373c51d92c134cdbe41eb
Colin <colin.diesh@gmail.com>
2021-05-19 18:16:07 -0400

    Updates

--- protein_schematic/url_builder/src/index.js
+++ protein_schematic/url_builder/src/index.js
@@ -1,39 +1,27 @@
+import { TabixIndexedFile } from "@gmod/tabix";
+import { LocalFile, RemoteFile, BlobFile } from "generic-filehandle";
+import fetch from "cross-fetch";
+
 console.log("hello");
 
 const protein = "JC8.10a";
-let lines;
-try {
-  lines = featList(protein);
-} catch (e) {
-  console.log(e);
-}
+(async () => {
+  const lines = await featList(protein);
+  console.log(lines.slice(0, 5));
+})();
 
-console.log(lines[0]);
-console.log(lines[1]);
-
-
-
- async function featList(protein) {
-  const { TabixIndexedFile } = require("@gmod/tabix");
-  const { LocalFile, RemoteFile, BlobFile } = require("generic-filehandle");
-  const { fetch } = require("node-fetch");
-
+async function featList(protein) {
   // use a remote file or other filehandle, note RemoteFile comes from https://github.com/GMOD/generic-filehandle
-  let remoteTbiIndexed;
-//  try{
-    remoteTbiIndexed = new TabixIndexedFile({
-      filehandle: new RemoteFile(
-        "https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz",
-        {fetch}
-      ),
-      tbiFilehandle: new RemoteFile(
-        "https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz.tbi",
-        {fetch}
-      ), // can also be csiFilehandle
-    });
-//  } catch (e) {
-//    console.log(e);
-//  }
+  const tbiIndexed = new TabixIndexedFile({
+    filehandle: new RemoteFile(
+      "https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz",
+      { fetch }
+    ),
+    tbiFilehandle: new RemoteFile(
+      "https://s3.amazonaws.com/agrjbrowse/test/c_elegans.PRJNA13758.WS280.protein_annotation.sorted.gff3.gz.tbi",
+      { fetch }
+    ),
+  });
 
   const lines = [];
   await tbiIndexed.getLines(protein, 0, undefined, function (line, fileOffset) {
@@ -41,4 +29,4 @@ console.log(lines[1]);
     lines.push(line);
   });
   return lines;
-};
+}
