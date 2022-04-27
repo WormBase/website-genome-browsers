1155706cd553377edf6c102193749d831ac419e0
Scott Cain <scott+git@scottcain.net>
2022-04-27 14:44:32 -0700

    added the ability to fetch the JB1 trackList to generate tracks from

--- jbrowse2/bin/make_tracks_config.pl
+++ jbrowse2/bin/make_tracks_config.pl
@@ -2,9 +2,9 @@
 use strict;
 use warnings;
 
+use JSON;
 use Getopt::Long;
 use FindBin qw($Bin);
-use JSON;
 #use Log::Log4perl;
 #Log::Log4perl->init("log4perl.conf");
 #my $log = Log::Log4perl->get_logger('build.log');
@@ -28,9 +28,12 @@ $ASSEMBLY ||= 'c_elegans_PRJNA13758';
 $S3URL    ||= 'https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase';
 
 #fetch trackList.json, parse into JSON object
+# the ".old" json file is the one that has the includes list
+# rather than the trackList.json that is used for JB 1, where
+# the includes have been inlined
 my $trackListLoc = "$S3URL/WS$RELEASE/$ASSEMBLY/trackList.json.old";
 
-#system("curl -O $trackListLoc") == 0 or die "failed fetching $trackListLoc: $!";
+system("curl -O $trackListLoc") == 0 or die "failed fetching $trackListLoc: $!";
 
 my $trackListFile = 'trackList.json.old';
 my $blob;
@@ -40,8 +43,6 @@ my $blob;
     $blob = <$F>;
     close $F;
 }
-
-my @files;
 
 my @tracks;
 
@@ -58,11 +59,15 @@ my $includedir = "$Bin/../config/track_configs";
 
 for my $f (@includes) {
     local $/ = undef;
+    if (!-e "$includedir/$f") {
+        warn "$includedir/$f not found\n";
+	next;
+    }
     open (my $F, "<", "$includedir/$f") or die "$!:$f";
     my $data = <$F>;
     close $F;
 
-    warn $f;
+    #warn $f;
 
     #do the substitutions, then json-ify
     $data =~ s/\$RELEASE/$RELEASE/g;
@@ -81,10 +86,12 @@ $$outputjson{tracks} = \@tracks;
 #print JSON->new->pretty(1)->encode($outputjson);
 
 
+# create a 'tracks' section for a server instance
 open (my $trackout, ">", $ASSEMBLY."_tracks.json") or die "$!";
 print $trackout JSON->new->pretty(1)->encode($outputjson);
 close $trackout;
 
+# create a tracks.js for an embedded instance
 open (my $embedtrackout, ">", $ASSEMBLY."_tracks.js") or die "$!";
 print $embedtrackout "  export default ";
 print $embedtrackout JSON->new->pretty(1)->encode(\@tracks);
